1df8f68f66c75c77876cb53f63c55a79883de4db
Stanislav Mayorov <minigamer97@gmail.com>
2016-06-20 17:21:48 +0600

    Урок 08 - Преобразования масштаба
    Изменения в коде минимальные, но не я такой, уроки такие.

--- lessons/main.cpp
+++ lessons/main.cpp
@@ -77,9 +77,9 @@ static void RenderSceneCB()
 
 	Matrix4f World;
 
-	World.m[0][0] = cosf(Scale)*cosf(Scale);	World.m[0][1] = -sinf(Scale)*cosf(Scale);	World.m[0][2] = 0.0;	World.m[0][3] = 0.0;
-	World.m[1][0] = sinf(Scale)*cosf(Scale);	World.m[1][1] = cosf(Scale)*cosf(Scale);	World.m[1][2] = 0.0;	World.m[1][3] = 0.0;
-	World.m[2][0] = 0.0;						World.m[2][1] = 0.0;						World.m[2][2] = 1.0;	World.m[2][3] = 0.0;
+	World.m[0][0] = sinf(Scale);	World.m[0][1] = -sinf(Scale);	World.m[0][2] = 0.0;	World.m[0][3] = 0.0;
+	World.m[1][0] = sinf(Scale);	World.m[1][1] = cosf(Scale);	World.m[1][2] = 0.0;	World.m[1][3] = 0.0;
+	World.m[2][0] = 0.0;						World.m[2][1] = 0.0;						World.m[2][2] = cosf(Scale);	World.m[2][3] = 0.0;
 	World.m[3][0] = 0.0;						World.m[3][1] = 0.0;						World.m[3][2] = 0.0;	World.m[3][3] = 1.0;
 
 	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]); // Плавно и красиво изменяем размер фигуры на экране
