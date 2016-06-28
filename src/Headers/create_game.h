d4598af2671d5e2a467134ec8d5e37c236252560
Stanislav Mayorov <minigamer97@gmail.com>
2016-06-28 14:53:43 +0600

    Удалил исходники игры Nim
    Будем честны, они мне оказались не нужны

--- src/Headers/create_game.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void create_game();
--- src/Headers/settings_config.h
+++
@@ -1,6 +0,0 @@
-#include "Bunches.h"
-// читает данные из config - файла
-void get_settings(Bunches &bun, char *str_music);
-
-// создаёт и записывает данные в config - файл
-void put_settings(int form, char str_music[50]);
