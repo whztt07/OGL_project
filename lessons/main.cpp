f4874379f3089d97f1e97c7b4528989fb1a30b42
Stanislav Mayorov <minigamer97@gmail.com>
2016-08-03 20:17:07 +0600

    Урок 44 - GLFW. Часть 2.
    Теперь, после добавления контента, изменим главный файл.

--- lessons/main.cpp
+++ lessons/main.cpp
@@ -1,43 +1,26 @@
 #include <math.h>
 #include <GL/glew.h>
-#include <GL/freeglut.h>
 #include <string>
-
-#include <float.h>
+#ifndef WIN32
+#include <sys/time.h>
+#include <unistd.h>
+#endif
+#include <sys/types.h>
 
 #include "ogldev_engine_common.h"
-#include "ogldev_util.h"
 #include "ogldev_app.h"
+#include "ogldev_util.h"
 #include "ogldev_pipeline.h"
 #include "ogldev_camera.h"
-#include "texture.h"
 #include "lighting_technique.h"
-#include "ogldev_glut_backend.h"
-#include "ogldev_basic_mesh.h"
-#include "shadow_map_technique.h"
-#include "shadow_cube_map_fbo.h"
+#include "ogldev_backend.h"
+#include "ogldev_camera.h"
+#include "mesh.h"
 
 using namespace std;
 
 #define WINDOW_WIDTH  1280  
 #define WINDOW_HEIGHT 1024
-
-struct CameraDirection
-{
-	GLenum CubemapFace;
-	Vector3f Target;
-	Vector3f Up;
-};
-
-CameraDirection gCameraDirections[NUM_OF_LAYERS] =
-{
-	{ GL_TEXTURE_CUBE_MAP_POSITIVE_X, Vector3f(1.0f, 0.0f, 0.0f),  Vector3f(0.0f, -1.0f, 0.0f) },
-	{ GL_TEXTURE_CUBE_MAP_NEGATIVE_X, Vector3f(-1.0f, 0.0f, 0.0f), Vector3f(0.0f, -1.0f, 0.0f) },
-	{ GL_TEXTURE_CUBE_MAP_POSITIVE_Y, Vector3f(0.0f, 1.0f, 0.0f),  Vector3f(0.0f, 0.0f, -1.0f) },
-	{ GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, Vector3f(0.0f, -1.0f, 0.0f), Vector3f(0.0f, 0.0f, 1.0f) },
-	{ GL_TEXTURE_CUBE_MAP_POSITIVE_Z, Vector3f(0.0f, 0.0f, 1.0f),  Vector3f(0.0f, -1.0f, 0.0f) },
-	{ GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, Vector3f(0.0f, 0.0f, -1.0f), Vector3f(0.0f, -1.0f, 0.0f) }
-};
 
 class Main : public ICallbacks, public OgldevApp
 {
@@ -46,18 +29,20 @@ public:
 	Main()
 	{
 		m_pGameCamera = NULL;
-		m_scale = 0.0f;
-		m_pointLight.AmbientIntensity = 0.1f;
-		m_pointLight.DiffuseIntensity = 0.9f;
-		m_pointLight.Color = Vector3f(1.0f, 1.0f, 1.0f);
-		m_pointLight.Attenuation.Linear = 0.0f;
-		m_pointLight.Position = Vector3f(0.0, 5.0, 0.0f);
+		m_directionalLight.Color = Vector3f(1.0f, 1.0f, 1.0f);
+		m_directionalLight.AmbientIntensity = 0.66f;
+		m_directionalLight.DiffuseIntensity = 1.0f;
+		m_directionalLight.Direction = Vector3f(1.0f, 0.0, 0.0);
 
-		m_persProjInfo.FOV = 90.0f;
+		m_persProjInfo.FOV = 60.0f;
 		m_persProjInfo.Height = WINDOW_HEIGHT;
 		m_persProjInfo.Width = WINDOW_WIDTH;
 		m_persProjInfo.zNear = 1.0f;
-		m_persProjInfo.zFar = 100.0f;
+		m_persProjInfo.zFar = 1000.0f;
+
+		m_pipeline.SetPerspectiveProj(m_persProjInfo);
+		m_pipeline.WorldPos(Vector3f(0.0f, 0.0f, 0.0f));
+		m_pipeline.Scale(0.1f, 0.1f, 0.1f);
 	}
 
 	virtual ~Main()
@@ -67,160 +52,55 @@ public:
 
 	bool Init()
 	{
-		Vector3f Pos(0.0f, 8.0f, -10.0f);
-		Vector3f Target(0.0f, -0.5f, 1.0f);
+		Vector3f Pos(0.0f, 23.0f, -5.0f);
+		Vector3f Target(-1.0f, 0.0f, 0.1f);
 		Vector3f Up(0.0, 1.0f, 0.0f);
-
-		if (!m_shadowMapFBO.Init(WINDOW_WIDTH, WINDOW_HEIGHT)) {
-			printf("Error initializing the shadow map FBO\n");
-			return false;
-		}
 
 		m_pGameCamera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT, Pos, Target, Up);
 
-		if (!m_lightingEffect.Init()) {
-			printf("Error initializing the lighting technique\n");
+		if (!m_LightingTech.Init()) {
+			OGLDEV_ERROR("Error initializing the lighting technique\n");
 			return false;
 		}
 
-		m_lightingEffect.Enable();
+		m_LightingTech.Enable();
 
-		m_lightingEffect.SetColorTextureUnit(COLOR_TEXTURE_UNIT_INDEX);
-		m_lightingEffect.SetShadowMapTextureUnit(SHADOW_TEXTURE_UNIT_INDEX);
-		m_lightingEffect.SetPointLight(m_pointLight);
+		m_LightingTech.SetColorTextureUnit(COLOR_TEXTURE_UNIT_INDEX);
+		m_LightingTech.SetDirectionalLight(m_directionalLight);
+		m_LightingTech.SetMatSpecularIntensity(0.0f);
+		m_LightingTech.SetMatSpecularPower(0);
 
-		if (!m_shadowMapEffect.Init()) {
-			printf("Error initializing the shadow map technique\n");
+		if (!m_mesh.LoadMesh("thirdparty/content/crytek_sponza/sponza.obj")) {
 			return false;
 		}
-
-		m_shadowMapEffect.Enable();
-		m_shadowMapEffect.SetLightWorldPos(m_pointLight.Position);
-
-		if (!m_quad.LoadMesh("thirdparty/content/quad.obj")) {
-			return false;
-		}
-
-		// Set the orientation of the two quads
-		m_quad1Orientation.m_scale = Vector3f(10.0f, 10.0f, 10.0f);
-		m_quad1Orientation.m_rotation = Vector3f(90.0f, 0.0f, 0.0f);
-
-		m_quad2Orientation.m_scale = Vector3f(10.0f, 10.0f, 10.0f);
-		m_quad2Orientation.m_pos = Vector3f(0.0f, 0.0f, 7.0f);
-
-		m_pGroundTex = new Texture(GL_TEXTURE_2D, "thirdparty/content/test.png");
-
-		if (!m_pGroundTex->Load()) {
-			return false;
-		}
-
-		if (!m_mesh.LoadMesh("thirdparty/content/sphere.obj")) {
-			return false;
-		}
-
-		m_mesh1Orientation.m_pos = Vector3f(0.0f, 3.0f, 0.0f);
-		m_mesh2Orientation.m_pos = Vector3f(0.0f, 5.0f, 3.0f);
-
-		glEnable(GL_TEXTURE_CUBE_MAP);
 
 		return true;
 	}
 
 	void Run()
 	{
-		GLUTBackendRun(this);
+		OgldevBackendRun(this);
 	}
 
 	virtual void RenderSceneCB()
 	{
-		CalcFPS();
-
-		m_scale += 0.05f;
-
 		m_pGameCamera->OnRender();
 
-		ShadowMapPass();
-		RenderPass();
-
-		RenderFPS();
-
-		glutSwapBuffers();
-	}
-
-	void ShadowMapPass()
-	{
-		glCullFace(GL_FRONT);
-
-		m_shadowMapEffect.Enable();
-
-		PersProjInfo ProjInfo;
-		ProjInfo.FOV = 90.0f;
-		ProjInfo.Height = WINDOW_HEIGHT;
-		ProjInfo.Width = WINDOW_WIDTH;
-		ProjInfo.zNear = 1.0f;
-		ProjInfo.zFar = 100.0f;
-
-		Pipeline p;
-		p.SetPerspectiveProj(m_persProjInfo);
-
-		glClearColor(FLT_MAX, FLT_MAX, FLT_MAX, FLT_MAX);
-
-		for (uint i = 0; i < NUM_OF_LAYERS; i++) {
-			m_shadowMapFBO.BindForWriting(gCameraDirections[i].CubemapFace);
-			glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-			p.SetCamera(m_pointLight.Position, gCameraDirections[i].Target, gCameraDirections[i].Up);
-
-			p.Orient(m_mesh1Orientation);
-			m_shadowMapEffect.SetWorld(p.GetWorldTrans());
-			m_shadowMapEffect.SetWVP(p.GetWVPTrans());
-			m_mesh.Render();
-
-			p.Orient(m_mesh2Orientation);
-			m_shadowMapEffect.SetWorld(p.GetWorldTrans());
-			m_shadowMapEffect.SetWVP(p.GetWVPTrans());
-			m_mesh.Render();
-		}
-	}
-
-	void RenderPass()
-	{
-		glCullFace(GL_BACK);
-
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		m_lightingEffect.Enable();
-		m_shadowMapFBO.BindForReading(SHADOW_TEXTURE_UNIT);
-		m_lightingEffect.SetEyeWorldPos(m_pGameCamera->GetPos());
+		m_LightingTech.SetEyeWorldPos(m_pGameCamera->GetPos());
 
-		Pipeline p;
-		p.SetPerspectiveProj(m_persProjInfo);
-		p.SetCamera(*m_pGameCamera);
+		m_pipeline.SetCamera(*m_pGameCamera);
 
-		// Render the quads
-		m_pGroundTex->Bind(COLOR_TEXTURE_UNIT);
-		p.Orient(m_quad1Orientation);
-		m_lightingEffect.SetWorldMatrix(p.GetWorldTrans());
-		m_lightingEffect.SetWVP(p.GetWVPTrans());
-		m_quad.Render();
+		m_LightingTech.SetWVP(m_pipeline.GetWVPTrans());
+		m_LightingTech.SetWorldMatrix(m_pipeline.GetWorldTrans());
 
-		p.Orient(m_quad2Orientation);
-		m_lightingEffect.SetWorldMatrix(p.GetWorldTrans());
-		m_lightingEffect.SetWVP(p.GetWVPTrans());
-		m_quad.Render();
-
-		// Render the meshes
-		p.Orient(m_mesh1Orientation);
-		m_lightingEffect.SetWorldMatrix(p.GetWorldTrans());
-		m_lightingEffect.SetWVP(p.GetWVPTrans());
 		m_mesh.Render();
 
-		p.Orient(m_mesh2Orientation);
-		m_lightingEffect.SetWorldMatrix(p.GetWorldTrans());
-		m_lightingEffect.SetWVP(p.GetWVPTrans());
-		m_mesh.Render();
+		// RenderFPS();     
+		CalcFPS();
+
+		OgldevBackendSwapBuffers();
 	}
 
 	virtual void KeyboardCB(OGLDEV_KEY OgldevKey)
@@ -228,7 +108,7 @@ public:
 		switch (OgldevKey) {
 		//case OGLDEV_KEY_ESCAPE:
 		case OGLDEV_KEY_q:
-			GLUTBackendLeaveMainLoop();
+			OgldevBackendLeaveMainLoop();
 			break;
 		default:
 			m_pGameCamera->OnKeyboard(OgldevKey);
@@ -242,38 +122,40 @@ public:
 
 private:
 
-	LightingTechnique m_lightingEffect;
-	ShadowMapTechnique m_shadowMapEffect;
+	LightingTechnique m_LightingTech;
 	Camera* m_pGameCamera;
-	float m_scale;
-	PointLight m_pointLight;
-	BasicMesh m_mesh;
-	BasicMesh m_quad;
-	Orientation m_quad1Orientation;
-	Orientation m_quad2Orientation;
-	Orientation m_mesh1Orientation;
-	Orientation m_mesh2Orientation;
+	DirectionalLight m_directionalLight;
+	Mesh m_mesh;
 	PersProjInfo m_persProjInfo;
-	Texture* m_pGroundTex;
-	ShadowCubeMapFBO m_shadowMapFBO;
+	Pipeline m_pipeline;
 };
 
 int main(int argc, char** argv)
 {
 	Magick::InitializeMagick(*argv);
-	GLUTBackendInit(argc, argv, true, false);
 
-	if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false, "OGLDEV Tutorials")) {
+	OgldevBackendInit(OGLDEV_BACKEND_TYPE_GLFW, argc, argv, true, false);
+
+	if (!OgldevBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false, "OGLDEV Tutorials")) {
+		OgldevBackendTerminate();
 		return 1;
 	}
 
-	Main App;
+	SRANDOM;
 
-	if (!App.Init()) {
+	Main* pApp = new Main();
+
+	if (!pApp->Init()) {
+		delete pApp;
+		OgldevBackendTerminate();
 		return 1;
 	}
 
-	App.Run();
+	pApp->Run();
+
+	delete pApp;
+
+	OgldevBackendTerminate();
 
 	return 0;
 }
